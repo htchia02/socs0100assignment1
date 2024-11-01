ed12a6404425c8222072a02462020cc5b3d2b3a0
htchia02 <heangtheng02@gmail.com>
2024-11-01 19:08:06 +0000

    update

--- Assignment1.R
+++ Assignment1.R
@@ -4,13 +4,71 @@ if (!require("pacman")) {
 }
 
 pacman::p_load(
-  tidyverse, # tidyverse pkgs including purrr
+  tidyverse, # tidyverse packages
   kableExtra,#table
   flextable, #table
   glue, #combining strings and objects
-  ggplot2) #dataviz
+  ggplot2,
+  skimr) #data visualisation
 
 #import data
 data <- read.csv("https://github.com/owid/covid-19-data/raw/refs/heads/master/public/data/cases_deaths/full_data.csv", header = TRUE)
 
-data
+skim(data)
+
+# Data wrangling - Number of Cases by Continent
+continents <- c("Africa","Asia","Europe",
+                "North America","Oceania","South America")
+
+top_cases_continents <- data %>% # Create new data frame to the existing data
+  filter(location %in% continents) %>% # Filter out the data for the continents
+  group_by(location)%>% # Group by location
+  filter(date == max(date))%>% 
+  select(total_cases)%>%
+  arrange(desc(total_cases))%>%
+  head(10)
+
+top_cases_continents
+
+# Data wrangling - Proportion of cases by country income
+
+country_income <- c("High-income countries", "Low-income countries", 
+                  "Lower-middle-income countries","Upper-middle-income countries")
+
+proportion_cases_income <- data %>%
+  filter(location %in% country_income) %>%
+  group_by(date, location) %>%
+  summarise(total_cases_by_income = sum(total_cases, na.rm = TRUE))
+
+world_cases <- data %>%
+  filter(location == "World") %>%
+  select(date, world_total_cases = total_cases)
+
+proportion_cases <- proportion_cases_income %>%
+  left_join(world_cases, by = "date") %>%
+  mutate(proportion = total_cases_by_income / world_total_cases)
+
+proportion_cases
+
+# Data wrangling - Rate of weekly case and death increase
+data <- data %>%
+  arrange(location,date)%>%
+  group_by(location)%>%
+  mutate(weekly_case_increase = ((weekly_cases - lag(weekly_cases)) / lag(weekly_cases)) *100,
+         weekly_death_increase = ((weekly_deaths - lag(weekly_deaths)) / lag(weekly_deaths)) *100)
+
+data %>%
+  select(date,weekly_case_increase,weekly_death_increase)
+
+
+# Data wrangling - Case Fatality Rate (CFR)
+data <- data %>%
+  arrange(date)%>%
+  group_by(location)%>%
+  mutate(cfr = (total_deaths/total_cases) * 100)
+
+data %>%
+  select(date,total_deaths,total_cases,cfr)
+
+# Function
+
